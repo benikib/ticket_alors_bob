8236d629de8baa086f5b8e16bd229050856e5e1e
marien <mmanima322@gmail.com>
2025-07-11 16:55:36 -0700

    feat scane2

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\TicketController;
 
-Route::get('/', function () {
-    return view('ticket.index');
-});
 
+Route::get('/', [TicketController::class, 'index'])->name("ticket.index");
 Route::get('tickets/', [TicketController::class, 'index'])->name("ticket.index");
 Route::get('/ticket/scanne', [TicketController::class, 'scanne'])->name("ticket.scanne");
 Route::get('/ticket/verify', [TicketController::class, 'verify'])->name("ticket.verify");
