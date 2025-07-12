0b7df5b13a70bb3813527b8bc26eef74c7e22649
marien <mmanima322@gmail.com>
2025-07-11 21:59:26 -0700

    feat ajout billet manuelement

--- app/Http/Controllers/admin/TicketController.php
+++ app/Http/Controllers/admin/TicketController.php
@@ -15,6 +15,30 @@ class TicketController extends Controller
         return View("ticket.index",["tickets"=>$tickets]);
         
      }
+     public function store(Request $request)
+    {
+
+        try {
+            
+            $now = time(); 
+            $start1978 = strtotime('1978-01-01 00:00:00'); 
+            $secondsSince1978 = $now - $start1978;
+            $code = 'Ticket-' . $secondsSince1978;
+    
+            $ticket = Ticket::create([
+                'nom'      => $request->nom,
+                'conctat'  => $request->conctat,
+                'n_billet' => $request->n_billet,
+                'vip'      => $request->vip, 
+                'code'     => $code,
+            ]);
+    
+            return redirect()->route('ticket.index');
+
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
 
      public function scanne(){
         return View("ticket.scanner");
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\admin\TicketController;
 
 Route::get('/', [TicketController::class, 'index'])->name("ticket.index");
 Route::get('tickets/', [TicketController::class, 'index'])->name("ticket.index");
+Route::post('tickets/', [TicketController::class, 'store'])->name("ticket.store");
 Route::get('/ticket/scanne', [TicketController::class, 'scanne'])->name("ticket.scanne");
 Route::post('/ticket/verify', [TicketController::class, 'verify'])->name("ticket.verify");
 
